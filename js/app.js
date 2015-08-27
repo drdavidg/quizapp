54998d43bab9c31f84e5e766c93b5ec1fed1520b
David Gonen <david@curiousminds.com>
2015-08-27 14:50:58 +0300

    got timer working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
+	timeRemaining();
+	function timeRemaining() {
+		$(".meter > span").each(function() {
+		  $(this)
+		    .data("origWidth", $(this).width())
+		    .width("origWidth")
+		    .animate({
+		      // width: $(this).data("origWidth") // or + "%" if fluid
+					width: 0
+		    }, 10000);
+		});
+	//make time left tick downwards.
+	var x = 9;
+	setInterval(function() {
+		if (x >= 0) {
+			$('.secondsleft').text(x);
+			x--;
+		}
+		else return;
+	}, 1000);
 
-
-	$(".meter > span").each(function() {
-	  $(this)
-	    .data("origWidth", $(this).width())
-	    .width(0)
-	    .animate({
-	      width: $(this).data("origWidth") // or + "%" if fluid
-	    }, 2000);
-	});
+}
 
 
 });
