a14a5d3480788e73cc1a13ff2e03d9508de3b654
David Gonen <david@curiousminds.com>
2015-08-27 16:29:38 +0300

    starting to write out micro functions

--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
 $(document).ready(function() {
 	timeRemaining();
 	function timeRemaining() {
-		$(".meter > span").each(function() {
+		$('.meter > span').each(function() {
 		  $(this)
-		    .data("origWidth", $(this).width())
-		    .width("origWidth")
+		    .data('origWidth', $(this).width())
+		    .width('origWidth')
 		    .animate({
 		      // width: $(this).data("origWidth") // or + "%" if fluid
 					width: 0
 		    }, 10000);
 		});
 	//make time left tick downwards.
-	var x = 9;
-	setInterval(function() {
-		if (x >= 0) {
-			$('.secondsleft').text(x);
-			x--;
-		}
-		else return;
-	}, 1000);
+	startTimer(9);
+	function startTimer(x) {
+		var timer = setInterval(function() {
+			if (x >= 0) {
+				$('.secondsleft').text(x);
+				x--;
+			}
+			else clearInterval(timer);
+		}, 1000);
+	}
 
 }
+var trivia = {
+	questions: ["What is Bran unable to do due to his injuries?", "Where is Theon Greyjoy tasked with raiding using only one ship by his father?" ],
+	choices: [["Sleep","Archery","Walk","Eat"],["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"]],
+	answer: ["Walk","Villages along the Stony Shore"]
+};
 
+var setQuestion = function(qnumber) {
+	$('.question').text(trivia.questions[qnumber]);
+};
+
+setQuestion(0);
+
+var setChoices = function(m) {
+	$('.multiplechoices div.q1 button').text(trivia.choices[m][0]);
+	$('.multiplechoices div.q2 button').text(trivia.choices[m][1]);
+	$('.multiplechoices div.q3 button').text(trivia.choices[m][2]);
+	$('.multiplechoices div.q4 button').text(trivia.choices[m][3]);
+};
+setChoices(0);
+
+var recordResult = function() {
+	question: []
+
+};
+
+var results = {
+	question: []
+}
 
 });
