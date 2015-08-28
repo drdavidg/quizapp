cfd82ba07220c09b06fc84780c89b625092d4448
David Gonen <david@curiousminds.com>
2015-08-28 18:08:02 +0300

    got timer progress bar AND time left text to stop on clicking any of the answer buttons.  a little challening, but figured out how to make the loop stop (used a variable outside the function as a flag

--- js/app.js
+++ js/app.js
@@ -12,63 +12,85 @@ $(document).ready(function() {
 		});
 	//make time left tick downwards.
 	startTimer(9);
+	}
+	var stop = true;
 	function startTimer(x) {
 		var timer = setInterval(function() {
-			if (x >= 0) {
+			if (x >= 0 && stop) {
 				$('.secondsleft').text(x);
 				x--;
 			}
 			else clearInterval(timer);
 		}, 1000);
 	}
+	function stopTimer() {
+		$('.meter > span').stop();
+		stop = false;
+	}
 
-}
-var trivia = {
-	questions: ["What is Bran unable to do due to his injuries?", "Where is Theon Greyjoy tasked with raiding using only one ship by his father?" ],
-	choices: [["Sleep","Archery","Walk","Eat"],["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"]],
-	answer: ["Walk","Villages along the Stony Shore"]
-};
+	var trivia = {
+		questions: ["What is Bran unable to do due to his injuries?", "Where is Theon Greyjoy tasked with raiding using only one ship by his father?" ],
+		choices: [["Sleep","Archery","Walk","Eat"],["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"]],
+		answer: ["Walk","Villages along the Stony Shore"]
+	};
 
-var setQuestion = function(qnumber) {
-	$('.question').text(trivia.questions[qnumber]);
-};
+	var setQuestion = function(qnumber) {
+		$('.question').text(trivia.questions[qnumber]);
+	};
 
-setQuestion(0);
+	setQuestion(0);
 
-var setChoices = function(m) {
-	$('.multiplechoices div.q1 button').text(trivia.choices[m][0]);
-	$('.multiplechoices div.q2 button').text(trivia.choices[m][1]);
-	$('.multiplechoices div.q3 button').text(trivia.choices[m][2]);
-	$('.multiplechoices div.q4 button').text(trivia.choices[m][3]);
-};
-setChoices(0);
+	var setChoices = function(m) {
+		$('.multiplechoices div.q1 button').text(trivia.choices[m][0]);
+		$('.multiplechoices div.q2 button').text(trivia.choices[m][1]);
+		$('.multiplechoices div.q3 button').text(trivia.choices[m][2]);
+		$('.multiplechoices div.q4 button').text(trivia.choices[m][3]);
+	};
+	setChoices(0);
 
-var recordResult = function(q, result) {
-	results.question[q] = result;
-};
+	$('.qcontent').on('click', '.multiplechoices div > button', function(event) { //listen for answer choice clicks
+		event.preventDefault();
+		/* Act on the event */
+		var choice = $( this ).text();
+		recordResult(0, choice);
+		setResults();
+		//should stop timer here somehow
+		stopTimer();
+		//should calculate # of points earned and update?
+		//move to next question after some delay or notice given to user
 
-var results = {
-	question: [0,0,1,1]
-};
+	});
 
-var setResults = function() {
-	var arrLength = results.question.length;
-		$('i').each(function(index){
-			if (results.question[index] === 0) {
-				$( this ).addClass('wronganswer fa-times-circle-o')
-				.removeClass('fa-circle-o')
-				.siblings('span').remove();
-			}
-			else if (results.question[index] === 1) {
-				$( this ).addClass('fa-check-circle-o rightanswer')
+	var recordResult = function(q, a) {
+		if (a === trivia.answer[q]) {
+			results.question[q] = 1;
+		}
+		else {
+			results.question[q] = 0;
+		}
+	};
+
+	var results = {
+		question: []
+	};
+
+	var setResults = function() {
+		var arrLength = results.question.length;
+			$('i').each(function(index){
+				if (results.question[index] === 0) {
+					$( this ).addClass('wronganswer fa-times-circle-o')
 					.removeClass('fa-circle-o')
 					.siblings('span').remove();
-			}
-			else if (index === arrLength) {
-				$( this ).addClass('currentquestion');
-			}
-		});
+				}
+				else if (results.question[index] === 1) {
+					$( this ).addClass('fa-check-circle-o rightanswer')
+						.removeClass('fa-circle-o')
+						.siblings('span').remove();
+				}
+				else if (index === arrLength) {
+					$( this ).addClass('currentquestion');
+				}
+			});
+	};
 
-};
-setResults();
 });
