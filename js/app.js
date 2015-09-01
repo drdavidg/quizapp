52a612a80844f66b782219fa9af067920fcee721
David Gonen <david@curiousminds.com>
2015-09-01 18:18:28 +0300

    refactored to use objects.  all the features so far seem to be working

--- js/app.js
+++ js/app.js
@@ -1,202 +1,225 @@
 $(document).ready(function() {
-	var results = {
-		question: []
-	};
-
-	var activeQuestion = function() { //ask Ryan about what i'm trying to do here, and why i'm getting errors that this function isn't defined
-		return results.question.length;
-	};
-
-
-	timeRemaining();
-	var initialWidth = $('.meter > span').width();
-
-	function timeRemaining(initialWidth) {
-		startTimer(9);
-		$('.meter > span').width(initialWidth);
-		$('.meter > span').each(function() {
-		  $(this)
-		    // .data('origWidth', $(this).width())
-		     .width('origWidth')
-		    .animate({
-					width: 0
-		    }, 10000);
-		});
-
-	}
-	var timer;
-	function startTimer(x) {
-		 timer = setInterval(function() {
-			if (x >= 0) {
-				$('.secondsleft').text(x);
-				x--;
-			}
-			else {
-				recordResult(activeQuestion());
-				setScoreboard();
-				clearInterval(timer);
-			}
-		}, 1000);
-	}
-	function stopTimer() {
-		$('.meter > span').stop();
-		clearInterval(timer);
-	}
-
-	var trivia = {
-		questions: ["What is Bran unable to do due to his injuries?", "Where is Theon Greyjoy tasked with raiding using only one ship by his father?", "As Maester Luwin is dying, he tells Osha to take Rickon and Bran Stark to " ],
-		choices: [["Sleep","Archery","Walk","Eat"],["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"],["Riverrun to find their mother","the Wall to find their brother Jon","Rivverun to find Rob", "King's Landing to find Arya and Sansa","find Benjen Stark north of the Wall"]],
-		answer: ["Walk","Villages along the Stony Shore","the Wall to find their brother Jon"]
-	};
-
-	var trivia2 = [
+	var quiz = [
 		{
 			prompt: "What is Bran unable to do due to his injuries?",
 			choices: ["Sleep","Archery","Walk","Eat"],
-			answer: "Walk"
+			answer: "Walk",
 		},
 		{
 			prompt: "Where is Theon Greyjoy tasked with raiding using only one ship by his father?",
-			choice: ["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"],
+			choices: ["Villages along the Stony Shore","Coastal towns of Bear Island","Ports along Blazewater Bay","Cape Kraken"],
 			answer: "Villages along the Stony Shore"
+		},
+		{
+			prompt: "As Maester Luwin is dying, he tells Osha to take Rickon and Bran Stark to",
+			choices: ["Riverrun to find their mother","the Wall to find their brother Jon","Rivverun to find Rob", "King's Landing to find Arya and Sansa","find Benjen Stark north of the Wall"],
+			answer: "the Wall to find their brother Jon"
+		},
+		{
+			prompt: "What does Arya earn after she helped the Hound in the fight with Polliver's men?",
+			choices: ["Horse","Armor","Chicken","Freedom"],
+			answer: "Horse"
+		},
+		{
+			prompt: "Who does Robb Stark send to make an alliance with Balon Greyjoy?",
+			choices: ["Catelyn","Bran","Lord Umber","Theon"],
+			answer: "Theon"
+		},
+		{
+			prompt: "Which city is Ser Loras Tyrell the heir to?",
+			choices: ["Riverrun","King's Landing","Highgarden","Winterfell"],
+			answer: "Highgarden"
+		},
+		{
+			prompt: "Who does Sansa beg to light a candle for her in Winterfell?",
+			choices: ["Theon","Podrick","Brienne","Roose"],
+			answer: "Theon"
+		},
+		{
+			prompt: "What is the number of Lord Commanders that have served when Jon is elected?",
+			choices: ["292","731","31","997"],
+			answer: "997"
+		},
+		{
+			prompt: "What does Bronn save Jaime from being killed by?",
+			choices: ["Jaguar","Boar","Rats","Snake"],
+			answer: "Snake"
+		},
+		{
+			prompt: "Who says the name of the episode in 'The Wars To Come'?",
+			choices: ["Mance","Daenerys","Tyrion","Sam"],
+			answer: "Mance"
 		}
 	];
-// trivia2[0]
-// trivia2[1]["prompt"]
 
-	var setQuestion = function(qnumber) {
-		$('.question').text(trivia.questions[qnumber]);
+	var gotTrivia = {
+		activeQuestion: function() {
+			var answeredQuestions = 0;
+				for (var r = 0; r < quiz.length; r++) {
+					if (quiz[r].result !== undefined) {
+						console.log(quiz[r].result);
+						console.log("added a question");
+						answeredQuestions++;
+					}
+				}
+				console.log("we're on question number " + answeredQuestions);
+			return answeredQuestions;
+		},
+		timeRemaining: function(initialWidth) {
+			gotTrivia.startTimer(9);
+			$('.meter > span').width(initialWidth);
+			$('.meter > span').each(function() {
+				$(this)
+					// .data('origWidth', $(this).width())
+					 .width('origWidth')
+					.animate({
+						width: 0
+					}, 10000);
+			});
+		},
+		startTimer: function(x) {
+				 timer = setInterval(function() {
+					if (x >= 0) {
+						$('.secondsleft').text(x);
+						x--;
+					}
+					else {
+						gotTrivia.recordResult(gotTrivia.activeQuestion());
+						gotTrivia.setScoreboard();
+						clearInterval(timer);
+					}
+				}, 1000);
+			},
+			stopTimer: function() {
+				$('.meter > span').stop();
+				clearInterval(timer);
+			},
+			setQuestion: function(qnumber) {
+				$('.question').text(quiz[qnumber].prompt);
+			},
+			setChoices: function(mchoice) {
+				var mchoices = quiz[mchoice].choices;
+				for (var z = 0; z < mchoices.length; z++) {
+					$('.multiplechoices div.q' + (z+1) + ' button').text(mchoices[z]);
+					$('.multiplechoices div.q' + (z+1) + ' button').removeClass('hider');
+					$('.multiplechoices div.q' + (z+1) + ' button').removeClass('incorrect').removeClass('correct');
+				}			},
+			setButtons: function(choice) {
+				$('.multiplechoices div > button').prop('disabled', true);
+				setTimeout(function() {
+					setCorrect(gotTrivia.activeQuestion());
+				}, 500);
+				setTimeout(function() {
+					hideIncorrect();
+				}, 1800);
+				setTimeout(function() {
+					hideIncorrectChoice();
+				}, 1900);
+				setTimeout(function() {
+					gotTrivia.nextQuestion();
+				}, 3000);
+
+				function setCorrect(active) {
+					$('.multiplechoices div > button').each(function(index){
+
+							if (($( this ).text()) === (quiz[(active-1)].answer)) {
+								$( this ).addClass('correct');
+							}
+					});
+				}
+				function hideIncorrectChoice() {
+					$('.multiplechoices div > button').each(function(index){
+						if ($( this ).text() === choice) {
+							$( this ).addClass('hider');
+						}
+					});
+				}
+				function hideIncorrect() {
+					$('.multiplechoices div > button').each(function(index){
+						if (($( this ).text() !== quiz[gotTrivia.activeQuestion()].answer) && ($( this ).text() !== choice)) {
+							$( this ).addClass('hider');
+						}
+					});
+				}
+			},
+			calcScore: function(result) {
+				//rules: 10 points per correct answer
+				//2 extra points if answered in <5 seconds
+				if (result) {
+					score += 10;
+					var secondsPoints = parseInt($('.secondsleft').text(), 10);
+					if (secondsPoints > 5) {
+						score += 2;
+					}
+					$('.playerscore').text(score);
+				}
+				$('.playerscore').text(score);
+			},
+			recordResult: function(q,a) {
+				gotTrivia.setButtons(a);
+				if (a === quiz[q].answer) {
+					quiz[q].result = true;  //**DONE** change this to booleans
+					gotTrivia.calcScore(true);
+					return;
+				}
+				else {
+					quiz[q].result = false;
+					return "incorrect";
+				}
+			},
+			setImage: function() {
+				$('.qimage > img').prop('src', 'img/' + (gotTrivia.activeQuestion()+1)  +'.jpg ');
+			},
+			setScoreboard: function() {
+				var arrLength = quiz.length;
+					$('i').each(function(index){
+						if (quiz[index].result === false) {
+							$( this ).addClass('wronganswer fa-times-circle-o')
+							.removeClass('fa-circle-o')
+							.siblings('span').hide(); //**DONE** instead of remove, maybe use .empty() so I can restart the quiz
+						}
+						else if (quiz[index].result) {
+							$( this ).addClass('fa-check-circle-o rightanswer')
+								.removeClass('fa-circle-o')
+								.siblings('span').remove();
+						}
+						else if (index === arrLength) {
+							$( this ).addClass('currentquestion');
+						}
+					});
+			},
+			nextQuestion: function(){
+				//hide question, all answers, picture.  the make new ones appear.  exit and enter with some sort of jquery animation
+				//have separate function that increments to the next question
+				gotTrivia.setQuestion(gotTrivia.activeQuestion());
+				gotTrivia.setChoices(gotTrivia.activeQuestion());
+				$('.qimage > img').prop('src', 'img/' + (gotTrivia.activeQuestion()+1)  +'.jpg ');
+				$('.multiplechoices div > button').prop('disabled', false);
+				$('.secondsleft').text(10);		//set timer to 10
+				gotTrivia.timeRemaining(initialWidth);
+			}
 	};
 
-	setQuestion(0);
+	gotTrivia.timeRemaining();
+	var initialWidth = $('.meter > span').width();
 
-	var setChoices = function(mchoice) {
-		var mchoices = trivia.choices[mchoice];
-		for (var z = 0; z < mchoices.length; z++) {
-			$('.multiplechoices div.q' + (z+1) + ' button').text(mchoices[z]);
-			$('.multiplechoices div.q' + (z+1) + ' button').removeClass('hider');
-			$('.multiplechoices div.q' + (z+1) + ' button').removeClass('incorrect').removeClass('correct');
-		}
-	};
-	setChoices(activeQuestion());
+	var timer;
+
+	gotTrivia.setQuestion(0);
+
+	gotTrivia.setChoices(gotTrivia.activeQuestion());
+
+	var score = 0;
+	gotTrivia.calcScore(false);
+
+	gotTrivia.setImage();
 
 	$('.qcontent').on('click', '.multiplechoices div > button', function(event) { //listen for answer choice clicks
 		event.preventDefault();
-		/* Act on the event */
-		var buttoncolor = recordResult(activeQuestion(), $(this).text());
+		var buttoncolor = gotTrivia.recordResult(gotTrivia.activeQuestion(), $(this).text());
 		$( this ).addClass(buttoncolor);
-
-		setScoreboard();
-		stopTimer();
-		//should calculate # of points earned and update?
-		//**DONE** mark question wrong if timer runs out
-		//move to next question after some delay or notice given to user
-		//DONE-if they chose wrong, make the button they chose red.  and make correct answer green.  after a pause wipe all noncorrect answers away.  also disable the buttons.
+		gotTrivia.setScoreboard();
+		gotTrivia.stopTimer();
 	});
-
-	var nextQuestion = function() {
-		//hide question, all answers, picture.  the make new ones appear.  exit and enter with some sort of jquery animation
-		//have separate function that increments to the next question
-		setQuestion(activeQuestion());
-		setChoices(activeQuestion());
-		$('.qimage > img').prop('src', 'img/' + (activeQuestion()+1)  +'.jpg ');
-		$('.multiplechoices div > button').prop('disabled', false);
-		$('.secondsleft').text(10);		//set timer to 10
-		timeRemaining(initialWidth);
-	};
-
-	var setButtons = function(choice) {
-
-		$('.multiplechoices div > button').prop('disabled', true);
-
-		setTimeout(function() {
-			setCorrect(results.question.length);
-		}, 500);
-		setTimeout(function() {
-			hideIncorrect();
-		}, 1800);
-		setTimeout(function() {
-			hideIncorrectChoice();
-		}, 1900);
-		setTimeout(function() {
-			nextQuestion();
-		}, 3000);
-
-
-		function setCorrect(active) {
-			$('.multiplechoices div > button').each(function(index){
-
-					if (($( this ).text()) === (trivia.answer[(active-1)])) {
-						$( this ).addClass('correct');
-					}
-			});
-		}
-		function hideIncorrectChoice() {
-			$('.multiplechoices div > button').each(function(index){
-				if ($( this ).text() === choice) {
-					$( this ).addClass('hider');
-				}
-			});
-		}
-		function hideIncorrect() {
-			$('.multiplechoices div > button').each(function(index){
-				if (($( this ).text() !== trivia.answer[activeQuestion()]) && ($( this ).text() !== choice)) {
-					$( this ).addClass('hider');
-				}
-			});
-		}
-
-	};
-	var score = 0;
-	var calcScore = function(result) {
-		//rules: 10 points per correct answer
-		//2 extra points if answered in <5 seconds
-		if (result) {
-			score += 10;
-			var secondsPoints = parseInt($('.secondsleft').text(), 10);
-			if (secondsPoints > 5) {
-				score += 2;
-			}
-			$('.playerscore').text(score);
-		}
-		$('.playerscore').text(score);
-	};
-	calcScore(false);
-
-	var recordResult = function(q, a) {
-		setButtons(a);
-		if (a === trivia.answer[q]) {
-			results.question[q] = true;  //**DONE** change this to booleans
-			calcScore(true);
-			return;
-		}
-		else {
-			results.question[q] = false;
-			return "incorrect";
-		}
-	};
-	var setImage = function() {
-		$('.qimage > img').prop('src', 'img/' + (activeQuestion()+1)  +'.jpg ');
-	};
-	setImage();
-
-	var setScoreboard = function() {
-		var arrLength = results.question.length;
-			$('i').each(function(index){
-				if (results.question[index] === false) {
-					$( this ).addClass('wronganswer fa-times-circle-o')
-					.removeClass('fa-circle-o')
-					.siblings('span').hide(); //**DONE** instead of remove, maybe use .empty() so I can restart the quiz
-				}
-				else if (results.question[index]) {
-					$( this ).addClass('fa-check-circle-o rightanswer')
-						.removeClass('fa-circle-o')
-						.siblings('span').remove();
-				}
-				else if (index === arrLength) {
-					$( this ).addClass('currentquestion');
-				}
-			});
-	};
 
 });
